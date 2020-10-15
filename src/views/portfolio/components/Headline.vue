6cc95e1370f689c32a31b0970a5e42c457d0dd48
Blas Oronoz <info@macizomedia.com>
2020-10-15 13:31:16 +0200

    first commit boiler plate vue vutify

---
+++ src/views/portfolio/components/Headline.vue
@@ -0,0 +1,23 @@
+<template>
+  <section
+    id="intro"
+    class="text-center mb-12"
+  >
+    <h1 class="display-2 font-weight-light mb-8">
+      My Portfolio
+    </h1>
+
+    <v-responsive
+      class="mx-auto subtitle-1 grey--text"
+      max-width="550"
+    >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum veniam atque quas eligendi officiis nemo at praesentium molestiae, veritatis doloremque delectus nostrum provident temporibus aliquid autem ipsam ducimus magnam porro.
+    </v-responsive>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'PortfolioHeadline',
+  }
+</script>
